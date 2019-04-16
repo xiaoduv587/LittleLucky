56a14922f9988a43d4c02dd31615a4786709d570
xiaodu <duzhibin-cf@motorc.com>
2019-04-16 18:00:17 +0800

    小幸运app。第一次提交

---
+++ app/src/main/java/com/xiaodu/littlelucky/App.kt
@@ -0,0 +1,15 @@
+package com.xiaodu.littlelucky
+
+import com.kotlin.base.common.BaseApplication
+
+/**
+ *
+ * @作者： xiaodu
+ * @时间： 2019/4/16
+ * @描述：
+ *
+ */
+class App : BaseApplication() {
+
+
+}
---
+++ app/src/main/java/com/xiaodu/littlelucky/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.xiaodu.littlelucky
+
+import com.kotlin.base.ui.activity.DefaultActivity
+
+class MainActivity : DefaultActivity() {
+
+
+    override fun setLayoutResID(): Int {
+        return R.layout.activity_main
+    }
+
+
+    override fun init() {
+    }
+
+
+}
